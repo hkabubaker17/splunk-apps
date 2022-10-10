e9943f79d2860355a51069f47dd52d14bba6faa8
hkabubaker17 <67294598+hkabubaker17@users.noreply.github.com>
2022-10-10 13:28:22 +0300

    Add files via upload

---
+++ TA-Sysmon-deploy/bin/deploy.ps1
@@ -0,0 +1,9 @@
+﻿#installing sysmon
+$path = "C:\Program Files\sysmon"
+New-Item $path -ItemType Directory
+Set-Location $path
+Invoke-WebRequest -Uri https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
+Expand-Archive Sysmon.zip -Force
+Set-Location $path\Sysmon
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/hkabubaker17/sysmon-config/main/sysmonconfig-export.xml -Outfile sysmonconfig-export.xml
+.\sysmon64.exe -accepteula -i sysmonconfig-export.xml
